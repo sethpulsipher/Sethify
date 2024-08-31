7db52db10dda9abce438bfac98aeaa49cc7dcb3a
seth poop <sethpulsipher@gmail.com>
2024-08-31 15:49:15 -0600

    Added 11ty navigation, plugins, post dates I think

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,35 @@
+// Imports
+const pluginEleventyNavigation = require("@11ty/eleventy-navigation");
+
+// Config
 const filterPostDate = require("./src/config/postDate");
 
 module.exports = function (eleventyConfig) {
 
+	/**=====================================================================
+                  PLUGINS - For additional eleventy functionality 
+    =======================================================================*/
+	eleventyConfig.addPlugin(pluginEleventyNavigation);
+
+
+	/**======================================================================
+       PASSTHROUGHS - Copy source files to /_site with no 11ty processing
+    ========================================================================*/
 	eleventyConfig.addPassthroughCopy("src/assets");
 	eleventyConfig.addPassthroughCopy("src/admin");
 
+
+	/**======================================================================
+             FILTERS - Modify data in template files at build time
+    ========================================================================*/
 	eleventyConfig.addFilter("postDate", filterPostDate);
+	/**======================================================================
+                SHORTCODES - Output data using JS at build time
+    ========================================================================*/
+	/**
+    *  Gets the current year, which can be outputted with {% year %}. Used for the footer copyright. Updates every build.
+    */
+	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
 	return {
 		dir: {
