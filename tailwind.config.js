f19688a85c2c0ae87ebcf590822208cdad7a44a2
seth poop <sethpulsipher@gmail.com>
2024-09-20 20:36:44 -0600

    safelist: checkmark

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,njk,js}", "./src/content/blog/*.md"],
+  content: [
+    "./src/**/*.{html,njk,js}", 
+    "./src/content/blog/*.md"
+  ],
+  safelist: [
+    'checkmark', // Add any other classes that might be purged
+  ],
   darkMode: 'class',
   theme: {
     extend: {}
