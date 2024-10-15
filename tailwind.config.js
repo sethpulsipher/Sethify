f28368e68f8d94612abc043810b9d21ffe7c6bb6
seth poop <sethpulsipher@gmail.com>
2024-10-14 19:50:45 -0600

    changed some tailwind configurations so I can include a normal css file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,6 @@ module.exports = {
     extend: {
       content: {
         checkmark: "url(../assets/svg/check.svg)",
-        swoopLight: "url(../assets/svg/swoop-light.svg)",
-        swoopDark: "url(../assets/svg/swoop-dark.svg)"
       },
     },
   },
