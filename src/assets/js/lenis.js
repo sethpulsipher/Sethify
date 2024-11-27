51f5e904ec7021b31a9d010d2ee8db4e10d6f2e9
seth poop <sethpulsipher@gmail.com>
2024-11-27 15:41:32 -0700

    (tailwindcss) : moved input.css outside of passThroughCopy

--- src/assets/js/lenis.js
+++ src/assets/js/lenis.js
@@ -1,10 +1,5 @@
 const lenis = new Lenis();
 
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
-
 // Use requestAnimationFrame to continuously update the scroll
 function raf(time) {
   lenis.raf(time);
