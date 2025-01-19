e73d5f94cf7c489d173f5f8d228181be481173e6
seth poop <sethpulsipher@gmail.com>
2025-01-19 14:32:44 -0700

    enhance tailwind configuration with custom font families and primary color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,28 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {
       content: {
         checkmark: "url(../svg/check.svg)",
       },
-      color: {
-        'slate-blue': "#010922",
+      colors: {
+        primary: {
+          "50": "#f1f4ff",
+          "100": "#e5e8ff",
+          "200": "#ced5ff",
+          "300": "#a7b1ff",
+          "400": "#767fff",
+          "500": "#3f42ff",
+          "600": "#2118ff",
+          "700": "#1007fa",
+          "800": "#0d05d2",
+          "900": "#0c06ac",
+          "950": "#000080"
+         }
       },
     },
   },
