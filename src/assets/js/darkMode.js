07eaba865e19c1f7ce4b6c8e533eeaa3b4088ab7
Seth Pulsipher <sethpulsipher@gmail.com>
2025-06-28 18:11:16 -0600

    changed to pnpm, header change, fonts added, darkmode.js & nav.js

--- src/assets/js/darkMode.js
+++ src/assets/js/darkMode.js
@@ -1,4 +1,3 @@
-/*
 // Icons
 const sunIcon = document.querySelector(".sun");
 const moonIcon = document.querySelector(".moon");
@@ -15,7 +14,7 @@ const iconToggle = () => {
 
 // Initial Theme Check - if user theme is dark then apply dark - else hide the sun icon
 const themeCheck = () => {
-    if(userTheme === "dark" || (!userTheme && darkSystemTheme)) {
+    if (userTheme === "dark" || (!userTheme && darkSystemTheme)) {
         document.documentElement.classList.add("dark");
         moonIcon.classList.add("display-none");
         return;
@@ -25,7 +24,7 @@ const themeCheck = () => {
 
 // Manual Theme Switch
 const themeSwitch = () => {
-    if(document.documentElement.classList.contains("dark")) {
+    if (document.documentElement.classList.contains("dark")) {
         document.documentElement.classList.remove("dark");
         localStorage.setItem("theme", "light");
         iconToggle();
@@ -47,52 +46,3 @@ moonIcon.addEventListener("click", () => {
 
 // invoke theme check on initial load
 themeCheck();
-*/
-
-
-//
-//    The Dark Mode System
-//
-
-/*
-// helper functions to toggle dark mode
-function enableDarkMode() {
-	document.documentElement.classList.add('dark');
-	localStorage.setItem('theme', 'dark');
-    document.querySelector('.sun').classList.remove('hidden'); // Show sun
-    document.querySelector('.moon').classList.add('hidden'); // Hide moon
-}
-function disableDarkMode() {
-	document.documentElement.classList.remove('dark');
-	localStorage.setItem('theme', 'light');
-    document.querySelector('.sun').classList.add('hidden'); // Hide sun
-    document.querySelector('.moon').classList.remove('hidden'); // Show moon
-}
-
-// determines a new users dark mode preferences
-function detectColorScheme() {
-	// default to the light theme
-	let theme = 'light';
-
-	// check localStorage for a saved 'theme' variable. if it's there, the user has visited before, so apply the necessary theme choices
-	if (localStorage.getItem('theme')) {
-		theme = localStorage.getItem('theme');
-	}
-	// if it's not there, check to see if the user has applied dark mode preferences themselves in the browser
-	else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		theme = 'dark';
-	}
-
-	// if there is no preference set, the default of light will be used. apply accordingly
-	theme === 'dark' ? enableDarkMode() : disableDarkMode();
-}
-
-// run on page load
-detectColorScheme();
-
-// add event listener to the dark mode button toggle
-document.getElementById('dark-mode-toggle').addEventListener('click', () => {
-	// on click, check localStorage for the dark mode value, use to apply the opposite of what's saved
-	localStorage.getItem('theme') === 'light' ? enableDarkMode() : disableDarkMode();
-});
-*/
--- src/assets/js/nav.js
+++ src/assets/js/nav.js
@@ -1,41 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Toggle mobile menu visibility
-    const menuToggleButton = document.getElementById('hs-navbar-alignment-collapse');
-    const mobileMenu = document.getElementById('hs-navbar-alignment');
-  
-    if (menuToggleButton && mobileMenu) {
-      menuToggleButton.addEventListener('click', function () {
-        mobileMenu.classList.toggle('hidden');
-      });
-    }
+var body = document.querySelector("body");
+const nav = document.querySelector("#navbar")
+const hamburgMenu = document.querySelector("#hamburgermenu");
 
-    // Handle dropdown toggle for each dropdown button
-    const dropdownButtons = document.querySelectorAll('[data-dropdown-toggle]');
-
-    dropdownButtons.forEach(button => {
-      button.addEventListener('click', function () {
-        const targetDropdown = document.querySelector(this.getAttribute('data-dropdown-toggle'));
-        if (targetDropdown) {
-          targetDropdown.classList.toggle('hidden');
-        }
-    });
-  });
+hamburgMenu.addEventListener('click', function() {
+  nav.classList.toggle('hidden')
 });
 
-function onToggleMenu(e) {
-  e.name = e.name === 'menu' ? 'close' : 'menu'
-}
-// This script shrinks the header after scrolling 100px
-/*
-document.addEventListener('scroll', () => {
-  const scroll = window.scrollY;
- 
- if (scroll >= 100) {
-     document.querySelector('header').classList.add('max-w-[80vh]', 'place-self-center');
- } else {
-     document.querySelector('header').classList.remove('max-w-[80vh]', 'place-self-center');
- }
-});
-*/
-
+// document.addEventListener('DOMContentLoaded', function () {
+//     //Toggle mobile menu visibility
+//     const hamburgerMenu = document.getElementById('toggle');
+//     const navbar = document.getElementById('navbar');
   
+//     if (hamburgerMenu && navbar) {
+//       hamburgerMenu.addEventListener('click', function () {
+//         navbar.classList.toggle('hidden');
+//       });
+//     }
+
+//     // Handle dropdown toggle for each dropdown button
+//     const dropdownButtons = document.querySelectorAll('[data-dropdown-toggle]');
+
+//     dropdownButtons.forEach(button => {
+//       button.addEventListener('click', function () {
+//         const targetDropdown = document.querySelector(this.getAttribute('data-dropdown-toggle'));
+//         if (targetDropdown) {
+//           targetDropdown.classList.toggle('hidden');
+//         }
+//     });
+//   });
+// });
